1e39863d93727a7544c7878a1b2519d414c40bcc
Vicente González Ruiz <vruiz@ual.es>
2023-10-03 23:04:07 +0200

    Update predict.py
    Signed-off-by: Vicente González Ruiz <vruiz@ual.es>

--- segment/predict.py
+++ segment/predict.py
@@ -206,7 +206,7 @@ def run(
                         #imc_masked[..., 0] = 255 * (1 - cpu_mask)
                         #imc_masked[..., 1] = 255 * (1 - cpu_mask)
                         #imc_masked[..., 2] = 255 * (1 - cpu_mask)
-                        save_one_box([0,0,cpu_mask.shape[1],cpu_mask.shape[0]], imc_masked, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+                        save_one_box([0,0,cpu_mask.shape[1],cpu_mask.shape[0]], imc_masked, file=save_dir / 'crops' / names[c] / f'{p.stem}_{j}.jpg', BGR=True)
                         print(p.stem, flush=True)
                         #else:
                         #    # This was here ...
