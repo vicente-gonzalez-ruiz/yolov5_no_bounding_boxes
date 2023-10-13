5f926a36df4ae9e7433ec8fb798cbbe73d0f4f8e
Vicente González Ruiz <vruiz@ual.es>
2023-10-13 12:44:39 +0200

    Update predict.py
    Signed-off-by: Vicente González Ruiz <vruiz@ual.es>

--- segment/predict.py
+++ segment/predict.py
@@ -195,7 +195,9 @@ def run(
                         # annotator.box_label(xyxy, label, color=colors(c, True)) # This line draw the bounding boxes and the labels
                         # end g0nx0s
                         # annotator.draw.polygon(segments[j], outline=colors(c, True), width=3)
-                        print(f'{p.stem}_{j} ', [int(i.numpy()) for i in xyxy], flush=True)
+                        print(f'{p.stem}_{j} ', *[int(i.numpy()) for i in xyxy], flush=True)
+                        with open(f'{p.stem}_{j}_crop.txt', 'a') as f:
+                            f.write(str(*[int(i.numpy()) for i in xyxy]) + '\n')
                     if save_crop:
                         # begin g0nx0s
                         #if len(det[:, :6]) > 1:
